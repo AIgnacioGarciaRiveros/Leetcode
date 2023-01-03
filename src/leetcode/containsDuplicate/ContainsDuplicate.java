a92ea01a006e77219b33823014e804fc9ee4f272
AIgnacioGarciaRiveros <garciariverosagustinignacio@gmail.com>
2023-01-03 19:40:12 -0300

    Add modification exercise

--- src/leetcode/containsDuplicate/ContainsDuplicate.java
+++ src/leetcode/containsDuplicate/ContainsDuplicate.java
@@ -5,15 +5,11 @@ import java.util.Arrays;
 public class ContainsDuplicate {
 
   public  boolean containsDuplicate(int[] nums) {
-      int i=0;
-      int j=i+1;
       Arrays.sort(nums);
-
-      for(i=0;i<nums.length-1;i++)
+      for(int i=0;i<nums.length-1;i++)
       {
-          if(nums[i]==nums[j])
+          if(nums[i]==nums[i+1])
               return true;
-          j++;
       }
       return false;
   }
