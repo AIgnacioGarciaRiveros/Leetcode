32432ee0afe8a99e4dd0905ba2e750f58b52eb73
AIgnacioGarciaRiveros <garciariverosagustinignacio@gmail.com>
2023-01-18 20:39:41 -0300

    Add twoSum exercise

--- src/leetcode/twoSum/TwoSum.java
+++ src/leetcode/twoSum/TwoSum.java
@@ -1,27 +1,40 @@
 package leetcode.twoSum;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class TwoSum {
 
     private  int[] twoSum(int[] nums, int target) {
-        int indexSolution[] = new int[2];
-        int i;
-        int j;
-        for (i = 0; i < nums.length; i++)
+        Map<Integer, Integer> complementOfNums = new HashMap<>();
+        int complement;
+        for (int i = 0; i < nums.length; i++)
         {
-            for (j=i+1;j<nums.length;j++)
+            complement=target-nums[i];
+            if(complementOfNums.containsKey(complement))
             {
-                if( nums[i]+nums[j] == target )
-                {
-                    indexSolution[0]=i;
-                    indexSolution[1]=j;
-                    break;
-                }
+                return new int[]{complementOfNums.get(complement),i};
             }
+            complementOfNums.put(nums[i],i);
         }
-        return indexSolution;
+        return null;
     }
 
     private  int[] twoSum2(int[] nums, int target) {
+        Map<Integer, Integer> complements = new HashMap<>();
+        Integer complementIndex;
+        for (int i = 0; i < nums.length; i++)
+        {
+            complementIndex=complements.get(nums[i]);
+            if(complementIndex != null)
+            {
+                return new int[]{i,complementIndex};
+            }
+            complements.put(target-nums[i],i);
+        }
+        return nums;
+    }
+    private  int[] twoSum3(int[] nums, int target) {
         int i;
         int j;
         for (i = 0; i < nums.length; i++)
