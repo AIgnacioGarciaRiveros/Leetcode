c5234ec20445d7e59eb3c0e65db922b3df16d28e
AIgnacioGarciaRiveros <garciariverosagustinignacio@gmail.com>
2023-01-19 22:21:07 -0300

    Add another solution exercise pivotIndex

--- src/leetcode/pivotIndex/PivotIndex.java
+++ src/leetcode/pivotIndex/PivotIndex.java
@@ -1,28 +1,47 @@
 package leetcode.pivotIndex;
 
 public class PivotIndex {
-    public int pivotIndex(int [] nums)
-    {
-        int sumLeft[] = new int [nums.length];
-        int sumRight[] = new int [nums.length];
+    public  int pivotIndex(int[] nums){
+        int sum=0;
+        int sumLeft= 0;
         int i;
-        int j;
+        for(i=0;i<nums.length;i++)
+            sum+=nums[i];
+
         for(i=0;i<nums.length;i++)
         {
-            for(j=0;j<nums.length;j++)
-            {
-                if(j<i)
-                {
-                    sumLeft[i]+=nums[j];
+            sum-=nums[i];
+            if(sumLeft==sum)
+                return i;
+
+            sumLeft+=nums[i];
+        }
+        return -1;
+    }
+
+    public int pivotIndex2(int[] nums) {
+        int sumLeft[] = new int[nums.length];
+        int sumRight[] = new int[nums.length];
+        int i;
+        int j;
+        for (i = 0; i < nums.length; i++) {
+            for (j = 0; j < nums.length; j++) {
+                if (j < i) {
+                    sumLeft[i] += nums[j];
                 }
 
-                if(j>i)
-                {
-                    sumRight[i]+=nums[j];
+                if (j > i) {
+                    sumRight[i] += nums[j];
                 }
 
-                if(sumLeft[i]==sumRight[i])
+                if (sumLeft[i] == sumRight[i])
                     return i;
             }
         }
+
+        return -1;
+    }
+
+
+
 }
