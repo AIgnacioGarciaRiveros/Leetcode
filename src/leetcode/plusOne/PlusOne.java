d94824a19a70144e7b01cdda894e4d528ec8bd63
AIgnacioGarciaRiveros <garciariverosagustinignacio@gmail.com>
2023-01-20 23:13:17 -0300

    Remove way of solving the exercise

--- src/leetcode/plusOne/PlusOne.java
+++ src/leetcode/plusOne/PlusOne.java
@@ -1,7 +1,5 @@
 package leetcode.plusOne;
 
-import java.math.BigInteger;
-
 public class PlusOne {
     public  int[] plusOne(int[] digits) {
         for(int i = digits.length-1; i>=0; i--){
@@ -14,21 +12,5 @@ public class PlusOne {
         int [] result = new int[digits.length+1];  // case 3: array containing all 9
         result[0] = 1;
         return result;
-    }
-    public  int[] plusOne2(int[] digits) {
-        String res="";
-        for(int i : digits){
-            res += String.valueOf(i);
-        }
-        BigInteger result=new BigInteger(res);
-        BigInteger p = new BigInteger("1");
-        result = result.add(p);
-        res = String.valueOf(result);
-        int ult[]= new int[res.length()];
-        int k = res.length();
-        for(int i=0; i<k;i++){
-            ult[i]=(Integer.valueOf(res.charAt(i))+2-50);
-        }
-        return ult;
     }
 }
